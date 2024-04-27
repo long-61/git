e027f345feb98f6bbe442b986c68d3b2aa5da81b
long-61 <164544612+long-61@users.noreply.github.com>
2024-04-27 10:53:59 -0400

    Add files via upload

---
+++ baby_gitt.py
@@ -0,0 +1,11 @@
+
+names = ["Gary", "Ray", "Jason", "DeeDee", "Hooray"]
+nums = [1, 2, 3]
+for name in names:
+    print(name)
+
+    for num in nums:
+        print(num)
+
+i_nums = iter(nums)
+print(i_nums)
